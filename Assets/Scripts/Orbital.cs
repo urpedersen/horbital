7baef1fc344123db13cc4a51eeebe12bba9796a2
Ulf R. Pedersen <ulf@urp.dk>
2017-03-06 18:07:35 +0100

    Added arbital prefab

---
+++ Assets/Scripts/Orbital.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbital : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject particle;
+    private GameObject[] particles;
+
+
+    [SerializeField]
+    private int numberOfParticles;
+
+    // Use this for initialization
+    void Start () {
+		for(int i=0;i < numberOfParticles; i++)
+        {
+            GameObject copy = Instantiate<GameObject>(particle);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
