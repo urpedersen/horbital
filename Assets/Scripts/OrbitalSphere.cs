9538eb8c25f168dc2d365c001ca3356aeaf1118d
Ulf R. Pedersen <ulf@urp.dk>
2017-03-20 20:53:34 +0100

    (some) Colors on phase

--- Assets/Scripts/OrbitalSphere.cs
+++ Assets/Scripts/OrbitalSphere.cs
@@ -7,19 +7,26 @@ public class OrbitalSphere : MonoBehaviour {
     private Renderer m_renderer;
 
     [SerializeField]
-    private float m_maxRadius = 1.0f;
+    private float m_maxRadius = 3.0f;
     [SerializeField]
-    private float m_step = 1.0f;
+    private float m_step = 0.05f;
 
     [SerializeField]
-    private int m_QuantumNumberN = 1;
+    private int m_QuantumNumberN = 2;
     [SerializeField]
     private int m_QuantumNumberL = 1;
     [SerializeField]
-    private int m_QuantumNumberM = 1;
+    private int m_QuantumNumberM = 0;
+
+    [SerializeField]
+    private float m_maxWavefunction = 3f;
+    [SerializeField]
+    private float m_minWavefunction = 0.5f;
+
 
     // Enables the programmer to se the value of the wave-function in Unity
     public float Output_wavefunction;
+    public float Output_wavefunctionAngle;
 
     // Use this for initialization
     void Start () {
@@ -37,34 +44,44 @@ public class OrbitalSphere : MonoBehaviour {
         transform.position = new Vector3(x+Random.Range(-m_step,m_step), y+Random.Range(-m_step, m_step), z+Random.Range(-m_step, m_step));
 
         // Compute wave function
-        float waveFunction = RadialWaveFunction() * AngularWaveFunctionPrefactor();
+        float waveFunctionNorm = wavefunctionNorm();
+        float waveFunctionAngle = wavefunctionAngle();
+        Output_wavefunction = waveFunctionNorm;
+        Output_wavefunctionAngle = waveFunctionAngle;
 
         // Change alpha
-        float alpha = numberToAlpha(waveFunction);
-        if (alpha < 0.001f) {
-            while (alpha < 0.001f)  // Find a location where the particle is not invisible
-            {
-                transform.position = new Vector3(Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius));
-                waveFunction = RadialWaveFunction()*AngularWaveFunctionPrefactor();
-                alpha = numberToAlpha(waveFunction);
-            }
+        float alpha = numberToAlpha(waveFunctionNorm, m_minWavefunction, m_maxWavefunction);
+        while (alpha < 0.001f)  // Find a location where the particle is not invisible
+        {
+            transform.position = new Vector3(Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius));
+            waveFunctionNorm = RadialWaveFunction()* AngularWaveFunctionReal();
+            alpha = numberToAlpha(waveFunctionNorm, m_minWavefunction, m_maxWavefunction);
         };
 
-
-        m_renderer.material.color = new Color(1.0f,0.0f,0.0f, alpha);
+        // Change color of wavefunction
+        if (waveFunctionNorm > 0f)
+        {
+            m_renderer.material.color = new Color(1.0f, 0.0f, 0.0f, alpha);
+        } else
+        {
+            m_renderer.material.color = new Color(0.0f, 0.0f, 1.0f, alpha);
+        }
         //Debug.Log(waveFunction);
 
-        Output_wavefunction = alpha;
+
 
         // Swop particle back if it move to far away
         if (r > m_maxRadius)
-            transform.position = new Vector3(Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius), Random.Range(-m_maxRadius, m_maxRadius));
-
+            transform.position = new Vector3(
+                Random.Range(-m_maxRadius, m_maxRadius), 
+                Random.Range(-m_maxRadius, m_maxRadius), 
+                Random.Range(-m_maxRadius, m_maxRadius)
+            );
     }
 
-    private float numberToAlpha(float input)
+    private float numberToAlpha(float input,float min,float max)
     {
-        float alpha = (input*input - 0.5f) / 2.0f;
+        float alpha = (input*input - min) / (max-min);
         if (alpha < 0f)
         {
             alpha = 0f;
@@ -73,6 +90,20 @@ public class OrbitalSphere : MonoBehaviour {
             alpha = 1.0f;
         }
         return alpha;
+    }
+
+    private float wavefunctionNorm()
+    {
+        return RadialWaveFunction()*AngularWaveFunctionPrefactor();
+    }
+
+    private float wavefunctionAngle()
+    {
+        float angle = (float)m_QuantumNumberM * Mathf.Atan2(this.transform.position.y, this.transform.position.x);
+        float pre = wavefunctionNorm();
+        if (wavefunctionNorm() < 0f)
+            angle -= Mathf.PI;
+        return angle;
     }
 
     private float RadialWaveFunction()
@@ -86,14 +117,14 @@ public class OrbitalSphere : MonoBehaviour {
 
 
         // TODO ensure that input is an allowed wave function
-        float result = Mathf.Pow(2f/n,3);
-        result *= (float)Factorial(n - l - 1)/2*n;
+        float result = Mathf.Pow(2f/n,3f);
+        result *= (float)Factorial(n - l - 1)/2f*n;
         float tmp = (float)Factorial(n + l);
         result /= tmp * tmp * tmp;
         result = Mathf.Sqrt(result);
         result *= Mathf.Exp(-r / (float)n);
-        result *= Mathf.Pow(2 * r / n, l);
-        result *= LaguerrePolynomials(2*l+1,n-l-1,2*r/n);
+        result *= Mathf.Pow(2f * r / n, l);
+        result *= AssociatedLaguerrePolynomials(2*l+1,n-l-1,2f*r/n);
 
         return result;
     }
@@ -108,7 +139,7 @@ public class OrbitalSphere : MonoBehaviour {
         // Compute spherical coordinates
         float phi = Mathf.Atan2(y, x);
 
-        return AngularWaveFunctionPrefactor() * Mathf.Cos(m * phi);
+        return AngularWaveFunctionPrefactor() * Mathf.Cos((float)m * phi);
     }
 
     private float AngularWaveFunctionImag()
@@ -121,7 +152,7 @@ public class OrbitalSphere : MonoBehaviour {
         // Compute spherical coordinates
         float phi = Mathf.Atan2(y, x);
 
-        return AngularWaveFunctionPrefactor() * Mathf.Sin(m * phi);
+        return AngularWaveFunctionPrefactor() * Mathf.Sin((float)m * phi);
     }
 
     // The anglar 
@@ -139,15 +170,16 @@ public class OrbitalSphere : MonoBehaviour {
         //float theta = Mathf.Acos(z / r);
         //float phi = Mathf.Atan2(y, x);
 
-        float output =1/(4*Mathf.PI);
+        float output =1f/(4f*Mathf.PI);
         output *= (float)Factorial(l + m);
-        output /= (2 * l + 1) * (float)Factorial(l - m);
-        output = Mathf.Pow(-1, m) / Mathf.Sqrt( output );
+        output /= (2f * l + 1f) * (float)Factorial(l - m);
+        output = Mathf.Pow(-1f, m) / Mathf.Sqrt( output );
         output *= AssociatedLegendrePolynomials() ;
 
         return output;
     }
 
+    // TODO Negative m's are missing
     private float AssociatedLegendrePolynomials()
     {
         int l = m_QuantumNumberL;
@@ -168,7 +200,7 @@ public class OrbitalSphere : MonoBehaviour {
         {
             if (m == 1)
             {
-                result = -Mathf.Sqrt(1 - arg * arg);
+                result = -Mathf.Sqrt(1f - arg * arg);
             }
             else if (m == 0)
             {
@@ -201,18 +233,21 @@ public class OrbitalSphere : MonoBehaviour {
                 result = 15f * arg * (1f - arg * arg);
             } else if (m == 1)
             {
-                result = -3f / 2f * (5f * arg * arg - 1) * Mathf.Sqrt(1f - arg * arg);
+                result = -3f / 2f * (5f * arg * arg - 1f) * Mathf.Sqrt(1f - arg * arg);
             } else if (m == 0)
             {
                 result = 0.5f * (5f * arg * arg * arg - 3f * arg);
             }
+        } else
+        {
+            Debug.LogError("AssociatedLegendrePolynomials() does not take so large aguments");
         }
 
         return result;
     }
 
 
-    private float LaguerrePolynomials(int up,int low,float x)
+    private float AssociatedLaguerrePolynomials(int up,int low,float x)
     {
         float result = 0f;
         if (up == 0)
@@ -222,11 +257,11 @@ public class OrbitalSphere : MonoBehaviour {
                 result = 1f;
             }else if (low == 1)
             {
-                result = -x + 1;
+                result = -x + 1f;
             }
             else if (low == 2)
             {
-                result = x*x-4f*x+2;
+                result = x*x-4f*x+2f;
             }
         }else if (up == 1)
         {
@@ -235,10 +270,10 @@ public class OrbitalSphere : MonoBehaviour {
                 result = 1f;
             } else if (low == 1)
             {
-                result = -2 * x + 4;
+                result = -2f * x + 4f;
             } else if (low == 2)
             {
-                result = 3 * x * x - 18 * x + 18;
+                result = 3f * x * x - 18f * x + 18f;
             }
         } else if (up == 2)
         {
@@ -247,23 +282,27 @@ public class OrbitalSphere : MonoBehaviour {
                 result = 2f;
             } else if (low == 1)
             {
-                result = -6 * x + 18;
+                result = -6f * x + 18f;
             } else if (low == 2)
             {
-                result = 12 * x * x - 96 * x + 144;
+                result = 12f * x * x - 96f * x + 144f;
             } 
         } else if (up == 3)
         {
             if ( low == 0)
             {
-                result = 6;
+                result = 6f;
             } else if (low == 1)
             {
-                result = -24 * x + 96;
+                result = -24f * x + 96f;
             } else if (low == 2)
             {
-                result = 60 * x * x - 600 * x + 1200;
+                result = 60f * x * x - 600f * x + 1200f;
             }
+        }
+        else
+        {
+            Debug.LogError("AssociatedLaguerrePolynomials() does not take so large aguments");
         }
         return result;
     }
